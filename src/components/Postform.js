72a2347307408c53ad4944c39c58a68878804e8f
mark <markpackham1@gmail.com>
2020-09-14 15:57:27 +0100

    remove obsolete fetch/post

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -23,16 +23,6 @@ export default class Postform extends Component {
       title: this.state.title,
       body: this.state.body,
     };
-
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(post),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
   }
 
   render() {
