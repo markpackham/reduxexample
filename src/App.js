a406080338dd76210f36ef307b336d0c86860db0
mark <markpackham1@gmail.com>
2020-09-14 12:26:12 +0100

    move onto Redux phase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./App.css";
 import Posts from "./components/Posts";
+import PostForm from "./components/Postform";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Using Redux in React learned from
-        https://www.youtube.com/watch?v=93p3LxR9xfM
-      </h1>
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>
+          Using Redux in React learned from
+          https://www.youtube.com/watch?v=93p3LxR9xfM
+        </h1>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
